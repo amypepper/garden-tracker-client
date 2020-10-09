f4fcefe870287beb7fb60c35c7a1e49b2b347694
amypepper <amycarlsonpepper@gmail.com>
2020-10-09 07:10:45 -0700

    add Landing component

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
+import Landing from "../Landing/Landing";
 import Navbar from "../Navbar/Navbar";
 import Signup from "../Signup/Signup";
 
@@ -9,36 +10,16 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Route path="/" component={Navbar} />
+          {["/", "/dashboard"].map((path, i) => (
+            <Route key={i} path={path} component={Navbar} />
+          ))}
+
           <h1>Garden Tracker</h1>
           <h2>Help your plants thrive.</h2>
         </header>
         <main className="App-main">
-          <section>
-            <h3>Track your activities</h3>
-            <p>[placeholder for screenshot of the activity list view]</p>
-            <p>
-              View a history of your plant care activities by logging in and
-              clicking on "View all activities."
-            </p>
-            <p>
-              You can sort the list by date, filter by plant name or activity
-              category.
-            </p>
-          </section>
-          <section>
-            <h3>Organize your activities</h3>
-            <p>[placeholder for category list view]</p>
-            <p>
-              Sort your activities into categories; you can add, remove or
-              rename categories at any time.
-            </p>
-            <p>
-              View all notes in a given category by clicking on the category
-              name in the lefthand sidebar.
-            </p>
-          </section>
-          <Route path="/" component={Signup} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/" component={Signup} />
         </main>
       </div>
     );
---
+++ src/Landing/Landing.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <>
+      <section>
+        <h3>Track your activities</h3>
+        <p>[placeholder for screenshot of the activity list view]</p>
+        <p>
+          View a history of your plant care activities by logging in and
+          clicking on "View all activities."
+        </p>
+        <p>
+          You can sort the list by date, filter by plant name or activity
+          category.
+        </p>
+      </section>
+
+      <section>
+        <h3>Organize your activities</h3>
+        <p>[placeholder for category list view]</p>
+        <p>
+          Sort your activities into categories; you can add, remove or rename
+          categories at any time.
+        </p>
+        <p>
+          View all notes in a given category by clicking on the category name in
+          the lefthand sidebar.
+        </p>
+      </section>
+    </>
+  );
+}
