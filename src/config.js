7cbc6728045d076615a3380f5fc0290c7199dd34
amypepper <amycarlsonpepper@gmail.com>
2021-01-27 16:12:00 -0800

    improve security measures

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
-  TOKEN_KEY: "garden-tracker-token",
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 };
